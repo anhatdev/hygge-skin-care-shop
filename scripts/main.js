172f5a5a54c92336e2772c69d91e6d8e119bc4c2
nhatanh17 <anhlnctk40@gmail.com>
2020-07-28 23:13:18 +0700

    store page & admin in progress

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,20 @@
-var selectList = document.querySelectorAll(".select__drop");
-selectList.forEach((select) => {
-  select.addEventListener("click", function (e) {
-    var dropDown = e.target.nextElementSibling;
-    e.target.classList.toggle("selectClick");
-    dropDown.classList.toggle("selectShow");
+var selectBtns = document.querySelectorAll(".select__drop");
+// Listen for all clicks on the document
+selectBtns.forEach((selectBtn) => {
+  selectBtn.addEventListener("click", function (e) {
+    var selectList = e.target.nextElementSibling;
+    var selectLabel = e.target.lastElementChild;
+    selectLabel.classList.toggle("selectLabel-js");
+    e.target.classList.toggle("selectBtn-js");
+    selectList.classList.toggle("selectList-js");
+
   });
 });
+// window.onclick = function (event) {
+//   if (!event.target.matches('.select__drop')) {
+
+//   }
+// }
+
+
+
